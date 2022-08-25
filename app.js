091476d43a5026485046881a7dc2658a91e74b8b
Lucas Coelho Lucinda <83791582+LucasWhatever@users.noreply.github.com>
2022-08-24 22:14:27 -0300

    alteraçoes 5

--- app.js
+++ app.js
@@ -85,6 +85,9 @@ app.use(express.static(path.join(__dirname, "public")))
 
 /* rotas */
 app.use('/', adm)
+app.get('/', (req, res) => {
+    res.render('adm/index')
+})
 /* rotas */
 
 /* outros */
